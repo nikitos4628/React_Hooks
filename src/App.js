1f8b4fe5f573637921dfd32a00dc9039c34313da
nikitos4628 <soba.nikita@mail.ru>
2023-03-14 07:41:00 +0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component, useState} from 'react';
+import {Component, useState, useEffect} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
@@ -11,6 +11,14 @@ import './App.css';
 //             slide: 0
 //         }
 //     }
+
+// 	 componentDidMount() {
+// 		document.title = `Slide: ${this.state.slide}`;
+// 	 }
+
+// 	 componentDidUpdate() {
+// 		document.title = `Slide: ${this.state.slide}`;
+// 	 }
 
 //     changeSlide = (i) => {
 //         this.setState(({slide}) => ({
@@ -47,25 +55,32 @@ import './App.css';
 //     }
 // }
 
-const calcValue = () => {
-	console.log('random');
 
-	return Math.random() * (50 - 1) + 1;
-}
 
 const Slider = (props) => {
 
-	const [slide, setSlide] = useState(() => calcValue());
+	const [slide, setSlide] = useState(0);
 	const [autoplay, setAutoplay] = useState(false);
-	// const [state, setState] = useState({slide: 0, autoplay: false});
 
-	// function changeSlide(i) {
-	// 	setState(state => ({...state, slide: state.slide + i}));
-	// }
+	function logging() {
+		console.log('log');
+	}
 
-	// function toggleAutoplay() {
-	// 	setState(state => ({...state, autoplay: !state.autoplay}));
-	// }
+	useEffect(() => {
+		console.log('effect');
+		document.title = `Slide: ${slide}`;
+
+		// window.addEventListener('click', logging);
+
+		// return () => {
+		// window.removeEventListener('click', logging);
+		// }
+
+	}, [slide]);
+
+	useEffect(() => {
+		console.log('autoplay');
+	}, [autoplay])
 
 	function changeSlide(i) {
 		setSlide(slide => slide + i);
@@ -98,8 +113,13 @@ const Slider = (props) => {
 
 
 function App() {
+	const [slider, setSlider] = useState(true);
+	
   return (
-        <Slider/>
+		<>
+		<button onClick={() => setSlider(false)}>click</button>
+			{slider ? <Slider/> : null}
+		</>  
   );
 }
 
