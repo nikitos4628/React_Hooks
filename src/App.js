ef46cb719979fc8b7d64f60fc8179130c8a07501
nikitos4628 <soba.nikita@mail.ru>
2023-03-14 10:37:21 +0700

    useRef

--- src/App.js
+++ src/App.js
@@ -1,89 +1,37 @@
-import {useState, useCallback, useEffect, useMemo} from 'react';
+import {useRef, useState, useEffect} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-const countTotal = (num) => {
-	console.log('counting...');
-	return num + 10;
-}
+const Form = () => {
+	const [text, setText] = useState('');
 
-const Slider = (props) => {
+    const myRef = useRef(1);
 
-	const [slide, setSlide] = useState(0);
-	const [autoplay, setAutoplay] = useState(false);
-
-	const getSomeImages = useCallback(() => {
-		console.log('fetching')
-		return [
-			'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg',
-			'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg'
-		]
-	}, [slide])
-
-	function changeSlide(i) {
-		setSlide(slide => slide + i);
-	}
-
-	function toggleAutoplay() {
-		setAutoplay(autoplay => !autoplay);
-	}
-
-	const total = useMemo(() => {
-		return countTotal(slide)
-	}, [slide]);
-
-	const style = useMemo(() => ({
-		color: slide > 4 ? 'red' : 'black'
-	}), [slide])
-
-	useEffect(() => {
-		console.log('styles!');
-	}, [style])
+	 useEffect(() => {
+		myRef.current = text;
+		
+	 })
 
     return (
         <Container>
-            <div className="slider w-50 m-auto">
-
-					<Slide getSomeImages={getSomeImages}/>
-
-                <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'autoplay' : null}</div>
-                
-					 <div style={style} className="text-center mt-5">Total slides: {total}</div>
-
-                <div className="buttons mt-3">
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => changeSlide(-1)}>-1</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => changeSlide(1)}>+1</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={toggleAutoplay}>toggle autoplay</button>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                    <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea value={myRef.current} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
-            </div>
+            </form>
         </Container>
     )
 }
 
-const Slide = ({getSomeImages}) => {
-	const [images, setImages] = useState([]);
-
-	useEffect(() => {
-		setImages(getSomeImages())
-	}, [getSomeImages])
-
-	return(
-		<>
-			{images.map((url, i) => <img key={i} className="d-block w-100" src={url} alt="slide" />)}
-		</>
-	)
-}
-
 function App() {
-  return (
-			<Slider/> 
-  );
+    return (
+        <Form/>
+    );
 }
 
 export default App;
